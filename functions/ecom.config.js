9cc8e654621f146665bb67b304d9193288e99454
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2024-01-06 17:12:37 -0300

    chore: set procedures to emit webhook on fultillment status change

--- functions/ecom.config.js
+++ functions/ecom.config.js
@@ -417,9 +417,9 @@ const app = {
 
 const procedures = []
 
-/**
- * Uncomment and edit code above to configure `triggers` and receive respective `webhooks`:
 
+ /** Uncomment and edit code above to configure `triggers` and receive respective `webhooks`:
+**/
 const { baseUri } = require('./__env')
 
 procedures.push({
@@ -429,42 +429,8 @@ procedures.push({
     // Receive notifications when new order is created:
     {
       resource: 'orders',
-      action: 'create',
-    },
-
-    // Receive notifications when order financial/fulfillment status are set or changed:
-    // Obs.: you probably SHOULD NOT enable the orders triggers below and the one above (create) together.
-    {
-      resource: 'orders',
-      field: 'financial_status',
-    },
-    {
-      resource: 'orders',
       field: 'fulfillment_status',
-    },
-
-    // Receive notifications when products/variations stock quantity changes:
-    {
-      resource: 'products',
-      field: 'quantity',
-    },
-    {
-      resource: 'products',
-      subresource: 'variations',
-      field: 'quantity'
-    },
-
-    // Receive notifications when cart is edited:
-    {
-      resource: 'carts',
-      action: 'change',
-    },
-
-    // Receive notifications when customer is deleted:
-    {
-      resource: 'customers',
-      action: 'delete',
-    },
+    }
 
     // Feel free to create custom combinations with any Store API resource, subresource, action and field.
   ],
@@ -481,7 +447,7 @@ procedures.push({
   ]
 })
 
- * You may also edit `routes/ecom/webhook.js` to treat notifications properly.
+ /* You may also edit `routes/ecom/webhook.js` to treat notifications properly.
  */
 
 exports.app = app
